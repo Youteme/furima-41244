f09237731e768d4ea232b3e3273f0a6af0931ec7
Youteme <j.yanase2@gmail.com>
2024-10-18 20:57:48 +0900

    商品出品機能②

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,6 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @items = Item.all.order(created_at: :desc)
   end
 
   def new
@@ -19,7 +18,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id,
+                                 :shipping_date_id, :price).merge(user_id: current_user.id)
   end
-
 end
