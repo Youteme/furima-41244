2fc26491b38e0b8a64b2e9f2da7da52f0180cb9f
Youteme <j.yanase2@gmail.com>
2024-10-19 13:21:26 +0900

    商品一覧表示機能①

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-
   root to: "items#index"
-  resources :items, only: [:index, :new, :create]
-
+  resources :items
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
